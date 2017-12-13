971092a9c118552bc79547ce7e80ec2d6eab8366
Alexey <shulzhenkoalexey@gmail.com>
2017-12-13 23:27:34 +0200

    changed back setup.bat file

--- proxy/setup.bat
+++ proxy/setup.bat
@@ -1,6 +1,4 @@
 @echo off
 cls
-rem C:\Users\Administrator\Downloads\nuget restore proxy.sln
-rem "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe" proxy.sln
-cd proxy
-dotnet run
+C:\Users\Administrator\Downloads\nuget restore proxy.sln
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe" proxy.sln
