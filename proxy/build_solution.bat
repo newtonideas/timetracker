3af5a4ec0dac4fd1a1461661816b43a274e61978
Andrii Brunets <brunets1997@gmail.com>
2018-03-26 17:38:05 +0300

    Batch file that builds the solution added

---
+++ proxy/build_solution.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+  "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" "proxy.sln" /t:restore
+  "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" "proxy.sln"
+)
