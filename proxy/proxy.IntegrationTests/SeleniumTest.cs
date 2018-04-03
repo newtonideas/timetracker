9c924156e7ff67335252b71e51a3b3533c21c4f8
Anton Sherstiuk <veselodum@gmail.com>
2018-04-03 11:03:18 +0300

    Addition of Selenium 2

--- proxy/proxy.IntegrationTests/SeleniumTest.cs
+++ proxy/proxy.IntegrationTests/SeleniumTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace proxy.IntegrationTests
 {
     [TestClass]
-    public class UnitTest1
+    public class SeleniumTest
     {
         [TestMethod]
         public void TestMethod1()
