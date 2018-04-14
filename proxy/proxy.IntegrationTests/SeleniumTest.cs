4264511192473f2bc3b1eeb86e1d11ff51a48a73
Anton Sherstiuk <veselodum@gmail.com>
2018-04-14 10:38:31 +0300

    Basic Selenium test on Google search page

--- proxy/proxy.IntegrationTests/SeleniumTest.cs
+++ proxy/proxy.IntegrationTests/SeleniumTest.cs
@@ -1,13 +1,39 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
 
 namespace proxy.IntegrationTests
 {
     [TestClass]
     public class SeleniumTest
     {
+        //reference for the browser
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Initialize() {
+            driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            
+        }
+
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void GoogleSearchTest() {
+            //navigation to the webpage
+            driver.Navigate().GoToUrl(@"https://www.google.com.ua/");
+            //element selection
+            IWebElement searchField = driver.FindElement(By.Name("q"));
+            //typing in the field
+            searchField.SendKeys("newton");
+
+            IWebElement searchBtn = driver.FindElement(By.Name("btnK"));
+            searchBtn.Submit();
+            
+        }
+
+        [TestCleanup]
+        public void Cleanup() {
+            driver.Close();
         }
     }
 }
