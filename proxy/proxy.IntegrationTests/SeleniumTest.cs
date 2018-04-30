92c77e213d175e289f29aa8c02465317b36c8dda
Anton Sherstiuk <veselodum@gmail.com>
2018-04-30 21:03:58 +0300

    Extended Selenium test

--- proxy/proxy.IntegrationTests/SeleniumTest.cs
+++ proxy/proxy.IntegrationTests/SeleniumTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 using System.IO;
 
 namespace proxy.IntegrationTests
@@ -28,7 +29,34 @@ namespace proxy.IntegrationTests
 
             IWebElement searchBtn = driver.FindElement(By.Name("btnK"));
             searchBtn.Submit();
+
+            IWebElement firstLink = driver.FindElement(By.XPath("(//h3)[1]/a"));
+            firstLink.Click();
+
+            IWebElement theoryOfColour = driver.FindElement(By.LinkText("theory of colour"));
+            theoryOfColour.Click();
+
+            driver.Navigate().Back();
             
+            IWebElement createAcc = driver.FindElement(By.Id("pt-createaccount"));
+            createAcc.Click();
+
+            driver.FindElement(By.Id("wpName2")).SendKeys("tti_newton");
+            driver.FindElement(By.Id("wpPassword2")).SendKeys("tti_newton");
+            driver.FindElement(By.Id("wpRetype")).SendKeys("tti_newton");
+            driver.FindElement(By.Id("wpEmail")).SendKeys("tti@newton.ideas");
+            
+            driver.FindElement(By.Id("pt-login")).Click();
+
+            driver.FindElement(By.Id("wpName1")).SendKeys("tti_newton");
+            driver.FindElement(By.Id("wpPassword1")).SendKeys("tti_newton");
+
+            driver.FindElement(By.Id("wpRemember")).Click();
+            driver.FindElement(By.Id("wpLoginAttempt")).Click();
+
+            driver.FindElement(By.LinkText("Log out")).Click();
+
+            driver.Navigate().GoToUrl(@"https://www.google.com.ua/search?q=newton");
         }
 
         [TestCleanup]
